a413ccd98755e6f4e873a378b70e1cbeaa3cb1e0
Rophy Tsai <rophy@users.noreply.github.com>
2024-03-08 07:43:28 +0800

    customize login form to disable username/password input

--- plugins/login-predefined.php
+++ plugins/login-predefined.php
@@ -1,6 +1,6 @@
 <?php
 
-/** Enable login with predefined user set in env vars
+/** Login with predefined user set in env vars
 * @link https://www.adminer.org/plugins/#use
 * @author Rophy Tsai <rophy123@gmail.com>
 * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
@@ -8,7 +8,12 @@
 class PredefinedLogin {
 
 	function credentials() {
-		return array(SERVER, $_ENV["ADMINER_PREDEFINED_PASSWORD"], $_ENV["ADMINER_PREDEFINED_PASSWORD"]);
+		return array(SERVER, $_ENV["ADMINER_PREDEFINED_USER"], $_ENV["ADMINER_PREDEFINED_PASSWORD"]);
+	}
+
+	function loginFormField($name, $heading, $value) {
+		if ($name == 'username' || $name == 'password') $value = 'Predefined <input type="hidden" name="auth['.$name.']" value="predefined">';
+		return $heading . $value;
 	}
 
 }
